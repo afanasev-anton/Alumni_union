9272da238a6320b585e4bf0bcddef336b5d53d3b
Anton Afanasev <afanasev.anton@icloud.com>
2020-04-15 17:49:16 +0200

    add pages, change IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\User;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,56 @@ class IndexController extends AbstractController
      */
     public function index()
     {
-        $posts = $this->getDoctrine()->getRepository
-        (Post::class)->findAll();
+        $posts = $this->getDoctrine()->getRepository('App:Post')->findAll();
 
         return $this->render('index/index.html.twig', array('posts' => $posts));
+    }
+
+    /**
+     * @Route("/alumni", name="alumni")
+     */
+    public function alumni()
+    {
+    	$users = $this->getDoctrine()->getRepository('App:User')->findAll();
+    	//findBy(['type'=>'alumni']); 
+        // add correct type from entity
+
+        return $this->render('pages/alumnies.html.twig', array('users' => $users));
+    }
+
+    /**
+     * @Route("/careers", name="career")
+     */
+    public function career()
+    {
+        $careers = $this->getDoctrine()->getRepository('App:Post')->findAll();
+        //findBy(['type'=>'offers']); 
+        // add correct type from entity
+
+        return $this->render('pages/careers.html.twig', array('careers' => $careers));
+    }
+
+    /**
+     * @Route("/events", name="event")
+     */
+    public function event()
+    {
+        $events = $this->getDoctrine()->getRepository('App:Post')->findAll();
+        //findBy(['type'=>'events']); 
+        // add correct type from entity
+
+        return $this->render('pages/events.html.twig', array('events' => $events));
+    }
+
+    /**
+     * @Route("/stories", name="story")
+     */
+    public function story()
+    {
+        $stories = $this->getDoctrine()->getRepository('App:Post')->findAll();
+        //findBy(['type'=>'stories']); 
+        // add correct type from entity
+
+        return $this->render('pages/stories.html.twig', array('stories' => $stories));
     }
 }
