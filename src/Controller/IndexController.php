1a1cd790386870f438141a527a9e6b3961c64186
Anton Afanasev <afanasev.anton@icloud.com>
2020-04-20 14:38:37 +0200

    add profile edit page and edit function into IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,6 +17,13 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// for the inputs
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class IndexController extends AbstractController
 {
@@ -73,13 +80,60 @@ class IndexController extends AbstractController
 
         $user = $this->getDoctrine()->getRepository('App:User')->find($id);
 
+        /** @var \App\Entity\User $user */
+        $userCurrent = $this->getUser();
+
+        return $this->render('pages/profile.html.twig', ['user' => $user, 'current' => $userCurrent]);
+    }
+    
+    /**
+     * @Route("/alumni/profile={id}/edit", name="profile_edit")
+     */
+    public function profileEdit($id, Request $request)
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $user = $this->getDoctrine()->getRepository('App:User')->find($id);
+
         // returns your User object, or null if the user is not authenticated
         // use inline documentation to tell your editor your exact User class
         /** @var \App\Entity\User $user */
         $userCurrent = $this->getUser();
-        $currentName = $userCurrent->getUsername();
+        $idCurrent = $userCurrent->getId();
+        //  id username roles password email first_name last_name has_job profile_pic skills github
+        
+        //Edit form
+        $form = $this->createFormBuilder($userCurrent)
+        ->add('firstName', TextType::class, array('label'=> 'First Name', 'attr' => array('class'=> 'form-control')))
+        ->add('lastName', TextType::class, array('label'=> 'Last Name', 'attr' => array('class'=> 'form-control')))
+        ->add('save', SubmitType::class, array('label'=> 'Update Profile', 'attr' => array('class'=> 'btn btn-primary w-25 mt-2')))
+        ->getForm();
+        $form->handleRequest($request);
 
-        return $this->render('pages/profile.html.twig', array('user' => $user, 'school' => 'CodeFactory', 'current' => $currentName));
+        if($form->isSubmitted() && $form->isValid()){
+            //fetching data
+            $FirstName = $form['firstName']->getData();
+            $LastName = $form['lastName']->getData();
+
+            //set attributes for User object
+            $userCurrent->setFirstName($FirstName);
+            $userCurrent->setLastName($LastName);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($userCurrent);
+            $entityManager->flush();
+            $this->addFlash(
+                'notice',
+                'User updated'
+            );
+            return $this->redirectToRoute('profile',['id'=>$idCurrent]);
+        }
+
+        return $this->render('pages/profile_edit.html.twig', [
+            'user' => $user, 
+            'current' => $userCurrent,
+            'form' => $form->createView()
+        ]);
     }
 
     /**
