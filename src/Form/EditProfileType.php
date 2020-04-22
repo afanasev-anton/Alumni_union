1984cfdb52e459788a5c3ddec0a7ec07ff4e87ca
Anton Afanasev <afanasev.anton@icloud.com>
2020-04-23 01:07:42 +0200

    change profile-edit page

--- src/Form/EditProfileType.php
+++ src/Form/EditProfileType.php
@@ -8,7 +8,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 // for the inputs
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+//use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -20,13 +21,28 @@ class EditProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
+            //->add('username')
             ->add('email')
-            ->add('firstName')
+            ->add('firstName'/*, TextType::class, [
+                'attr' => [
+                    'class' => 'form-control w-25',
+                    'placeholder' => 'First name'
+                ]
+            ]*/)
             ->add('lastName')
             ->add('hasJob')
             ->add('skills')
             ->add('github')
+            ->add('tel')
+            ->add('birth', DateType::class, [
+                'widget' => 'single_text',
+                // this is actually the default format for single_text
+                'format' => 'yyyy-MM-dd',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('course')
+            ->add('nation')
+            ->add('descr')
             ->add('imageFile', VichImageType::class, [
                 'required' => false,
                 'allow_delete' => true,
@@ -36,7 +52,7 @@ class EditProfileType extends AbstractType
             ->add('save', SubmitType::class, [
                 'label'=> 'Update Profile',
                 'attr' => [
-                    'class'=> 'btn btn-primary w-25 mt-2']
+                    'class'=> 'btn btn-primary w-100']
                 ]
             )
         ;
