511629c589a4b6b3081f75dbef1f514af26f4b16
Anton Afanasev <afanasev.anton@icloud.com>
2020-04-23 03:30:28 +0200

    add Profile Editor, fix profile page

--- src/Form/EditProfileType.php
+++ src/Form/EditProfileType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
@@ -22,32 +23,87 @@ class EditProfileType extends AbstractType
     {
         $builder
             //->add('username')
-            ->add('email')
-            ->add('firstName'/*, TextType::class, [
+            ->add('email', TextType::class, [
                 'attr' => [
-                    'class' => 'form-control w-25',
+                    'class' => 'form-control w-75 ml-2',
+                    'placeholder' => 'E-mail'
+                ]
+            ])
+            ->add('firstName', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control w-75 ml-auto',
                     'placeholder' => 'First name'
                 ]
-            ]*/)
-            ->add('lastName')
-            ->add('hasJob')
-            ->add('skills')
-            ->add('github')
-            ->add('tel')
+            ])
+            ->add('lastName', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control w-75 mr-auto',
+                    'placeholder' => 'Last name'
+                ]
+            ])
+            ->add('hasJob', CheckboxType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'custom-control-input'
+                ]
+            ])
+            ->add('skills', TextareaType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control w-75 ml-2',
+                    'placeholder' => 'your skills here...'
+                ]
+            ])
+            ->add('github', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control w-75 ml-2',
+                    'placeholder' => 'repository link'
+                ]
+            ])
+            ->add('tel', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control w-75 ml-2',
+                    'placeholder' => 'Telephone'
+                ]
+            ])
             ->add('birth', DateType::class, [
+                'required' => false,
                 'widget' => 'single_text',
                 // this is actually the default format for single_text
                 'format' => 'yyyy-MM-dd',
-                'attr' => ['class' => 'form-control'],
+                'attr' => ['class' => 'form-control w-50 ml-2'],
             ])
-            ->add('course')
-            ->add('nation')
-            ->add('descr')
+            ->add('course', TextareaType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control w-75 ml-2',
+                    'placeholder' => 'your course/es...'
+                ]
+            ])
+            ->add('nation', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control w-75 ml-2',
+                    'placeholder' => 'Nationality'
+                ]
+            ])
+            ->add('descr', TextareaType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control mx-auto',
+                    'placeholder' => 'write something about yourself...'
+                ]
+            ])
             ->add('imageFile', VichImageType::class, [
                 'required' => false,
-                'allow_delete' => true,
-                'download_uri' => true,
-                'image_uri' => true
+                'allow_delete' => false,
+                'download_uri' => false,
+                'image_uri' => true,
+                'attr' => [
+                    'class' => 'custom-file-input'
+                ]
             ])
             ->add('save', SubmitType::class, [
                 'label'=> 'Update Profile',
