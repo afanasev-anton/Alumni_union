1849a259503eafbb3ab15845f326cc746174021c
Patrick Bukenberger <patrick.bukenberger@gmail.com>
2020-04-23 11:49:22 +0200

    post search done

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Repository\PostRepository;
 use Symfony\Component\Form\Extension\Core\Type\{TextType, SubmitType};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -48,6 +49,50 @@ class SearchController extends AbstractController
         }
 
         return $this->render('pages/alumnies.html.twig', array('users' => $users));
+
+    }
+
+    /**
+     * @Route("/posts/search", name="post_search")
+     */
+
+    public function searchBarPosts()
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('handlePostSearch'))
+            ->add('searchPost', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control', 
+                    'placeholder' => 'search for posts'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-2'
+                ]
+            ])
+            ->getForm();
+            
+        return $this->render('search/searchBar.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/handlePostSearch", name="handlePostSearch")
+     * @param Request $request
+     */
+    public function handleSearchPosts(Request $request, PostRepository $postRepository)
+    {
+        $searchPost = $request->request->get('form')['searchPost'];
+
+        $tag = 'Searched';
+        
+        if ($searchPost) {
+            $posts = $postRepository->findByTitle($searchPost);
+        }
+
+        return $this->render('pages/showCategory.html.twig', array('posts' => $posts, 'tag' => $tag));
 
     }
 }
